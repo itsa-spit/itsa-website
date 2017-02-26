e4013d9d6bb66a3af08721c78431bf225a062f28
ashniu123 <nisheet1.sinvhal@gmail.com>
2017-02-26 23:40:08 +0530

    Proper indents as per Aashish's Orders

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,24 +1,24 @@
 $('#nav').affix({
-  offset: {
-    top: $('header').height()
-  }
+    offset: {
+        top: $('header').height()
+    }
 });
 
 $('#sidebar').affix({
-  offset: {
-    top: 260
-  }
+    offset: {
+        top: 260
+    }
 });
 
 /* smooth scrolling for scroll to top */
 $('.scroll-top').click(function(){
-  $('.scroll-top a').blur();
-  $('body,html').animate({scrollTop:0},1000);
+    $('.scroll-top a').blur();
+    $('body,html').animate({scrollTop:0},1000);
 });
 
 /* smooth scrolling for nav sections */
 $('#nav .navbar-nav li>a').click(function(){
-  var link = $(this).attr('href');
-  var posi = $(link).offset().top;
-  $('body,html').animate({scrollTop:posi},700);
+    var link = $(this).attr('href');
+    var posi = $(link).offset().top;
+    $('body,html').animate({scrollTop:posi},700);
 });
