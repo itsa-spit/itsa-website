935e7a5e45a20c01e9e9ced038cd2531f3afd9e3
ashniu123 <nisheet1.sinvhal@gmail.com>
2017-03-02 22:29:48 +0530

    Almost complete Navbar(No styling yet)

--- js/indexjs.js
+++ js/indexjs.js
@@ -2,8 +2,14 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 50) {
         $("#nav").addClass("darkNav");
+        $('#navbarLeft').addClass('leftNavbar');
+        $('#navbarRight').addClass('rightNavbar');
+        $('#ITSA').addClass('itsa-logo');
     } else {
         $("#nav").removeClass("darkNav");
+        $('#navbarLeft').removeClass('leftNavbar');
+        $('#navbarRight').removeClass('rightNavbar');
+        $('#ITSA').removeClass('itsa-logo');
     }
 });
 
