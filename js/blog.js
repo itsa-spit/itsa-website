41ca29c6b5eb9c8424c4ada9d20a6e8d0219d2a0
Aashish Nehete <ashnehete@gmail.com>
2017-03-03 17:15:15 +0530

    Merge branch 'master' of A:\Programming\itsa-website with conflicts.

---
+++ js/blog.js
@@ -0,0 +1,19 @@
+console.log("This");
+$.ajax({
+    url: 'blog/posts.json',
+    success: function (data) {
+        getPostCard(data[0], 0);
+    },
+    dataType: 'json'
+});
+
+function openPost(postSpan) {
+    console.log(postSpan.getAttribute("post-id"));
+}
+
+function getPostCard(post, id) {
+    var html = '<div class="card"><h3>' + post.title + '</h3><hr><p>' + post.desc + '</p>' +
+        '<span class="btn-link" onclick="openPost(this)" post-id="' + id +
+        '">See more...</span>' + '</div>';
+    $("#content").append(html);
+}
