15bc885bf362c05f6a04972ee34c507013939215
ashniu123 <nisheet1.sinvhal@gmail.com>
2017-03-18 23:25:21 +0530

    Revamp beginning

--- js/indexjs.js
+++ js/indexjs.js
@@ -53,3 +53,43 @@ $('#nav .navbar-nav li>a').click(function () {
 
 //Set year in footer automatically
 $('#currentYear').html(new Date().getFullYear());
+
+//Internet Explorer 10 in Windows Phone 8
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            '@-ms-viewport{width:auto!important}'
+        )
+    );
+    document.head.appendChild(msViewportStyle)
+}
+//Android stock Browser
+$(function () {
+    var nua = navigator.userAgent
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+    if (isAndroid) {
+        $('select.form-control').removeClass('form-control').css('width', '100%')
+    }
+});
+
+/*********Ripple Js********/
+$.ripple(".btn", {
+    debug: false, // Turn Ripple.js logging on/off
+    on: 'mousedown', // The event to trigger a ripple effect
+
+    opacity: 0.4, // The opacity of the ripple
+    color: "auto", // Set the background color. If set to "auto", it will use the text color
+    multi: false, // Allow multiple ripples per element
+
+    duration: 0.7, // The duration of the ripple
+
+    // Filter function for modifying the speed of the ripple
+    rate: function (pxPerSecond) {
+        return pxPerSecond;
+    },
+
+    easing: 'linear' // The CSS3 easing function of the ripple
+});
+/**********Material**********/
+$.material.init();
